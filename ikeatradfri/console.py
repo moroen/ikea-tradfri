ba144769d21fa5a88b031d9e4af8041f54866d20
moroen <moroen@gmail.com>
2019-03-29 01:13:30 +0100

    Refactored get_devices, intial gui

--- ikeatradfri/console.py
+++ ikeatradfri/console.py
@@ -1,30 +1,31 @@
-from .devices import getDevices
+from .devices import get_devices
 
 
-async def listDevices(api, gateway):
-    lights, outlets, groups, others = await getDevices(api, gateway)
+async def list_devices(api, gateway):
+    lights, outlets, groups, others = await get_devices(api, gateway)
     print("Lights:")
     for aDevice in lights:
         print("{0}: {1} ({2}) - {3}".
-              format(aDevice.id, aDevice.name,
-                     aDevice.device_info.model_number,
-                     aDevice.light_control.lights[0].state))
+              format(aDevice.device_id, aDevice.device_name,
+                     aDevice.device_model,
+                     aDevice.state))
 
     print("\nSockets:")
     for aDevice in outlets:
         print("{0}: {1} ({2}) - {3}".
-              format(aDevice.id, aDevice.name,
-                     aDevice.device_info.model_number,
-                     aDevice.socket_control.sockets[0].state))
+              format(aDevice.device_id, aDevice.device_name,
+                     aDevice.device_model,
+                     aDevice.state))
 
     print("\nDevices:")
     for aDevice in others:
         print("{0}: {1} ({2}) - {3}".
-              format(aDevice.id, aDevice.name,
-                     aDevice.device_info.model_number,
-                     aDevice.device_info.battery_level))
+              format(aDevice.device_id, aDevice.device_name,
+                     aDevice.device_model,
+                     aDevice.battery_level))
 
     print("\nGroups:")
     for aGroup in groups:
-        print("{0}: {1}".format(aGroup.id, aGroup.name))
+        print("{0}: {1}".format(
+            aGroup.description["DeviceID"], aGroup.description["Name"]))
     return
